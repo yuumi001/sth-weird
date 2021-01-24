0867cf89b6979ef10300fdac54ede23e848e2dbc
b4n4n4 <62524937+yuumi001@users.noreply.github.com>
2021-01-25 06:52:11 +0700

    Update one-for-all.sh

--- ubuntu/one-for-all.sh
+++ ubuntu/one-for-all.sh
@@ -3,7 +3,7 @@ sudo dpkg --add-architecture i386
 sudo add-apt-repository -y ppa:wireshark-dev/stable
 sudo add-apt-repository -y ppa:ubuntuhandbook1/audacity 	
 sudo apt-get update 
-sudo apt-get install -y build-essential jq strace ltrace curl wget ruby gcc dnsutils netcat gcc-multilib net-tools vim gdb gdb-multiarch python python3 python3-pip python3-dev libssl-dev libffi-dev wget git make procps libpcre3-dev libdb-dev libxt-dev libxaw7-dev python-pip libc6:i386 libncurses5:i386 libstdc++6:i386 ruby-dev wine64 wine32 wireshark audacity bless zip unzip
+sudo apt-get install -y build-essential jq strace ltrace curl wget ruby gcc dnsutils netcat gcc-multilib net-tools vim gdb gdb-multiarch python python3 python3-pip python3-dev libssl-dev libffi-dev wget git make procps libpcre3-dev libdb-dev libxt-dev libxaw7-dev python-pip libc6:i386 libncurses5:i386 libstdc++6:i386 ruby-dev wine64 wine32 wireshark audacity bless zip unzip nasm
 pip install capstone requests pwntools r2pipe ropgadget
 pip3 install pwntools keystone-engine unicorn==1.0.2rc1 capstone ropper ropgadget
 
