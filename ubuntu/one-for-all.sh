e6b940f4eea691908bf49bcc558973b78cdfbc2a
b4n4n4 <62524937+yuumi001@users.noreply.github.com>
2021-02-21 15:13:59 +0700

    Update one-for-all.sh

--- ubuntu/one-for-all.sh
+++ ubuntu/one-for-all.sh
@@ -3,10 +3,12 @@ sudo dpkg --add-architecture i386
 sudo add-apt-repository -y ppa:wireshark-dev/stable
 sudo add-apt-repository -y ppa:ubuntuhandbook1/audacity 	
 sudo apt-get update 
-sudo apt-get install -y build-essential jq strace ltrace curl wget ruby gcc dnsutils netcat gcc-multilib net-tools vim gdb gdb-multiarch python python3 python3-pip python3-dev libssl-dev libffi-dev wget git make procps libpcre3-dev libdb-dev libxt-dev libxaw7-dev python-pip libc6:i386 libncurses5:i386 libstdc++6:i386 ruby-dev wine64 wine32 wireshark audacity bless zip unzip nasm
+sudo apt-get install -y zsh build-essential jq strace ltrace curl wget ruby gcc dnsutils netcat gcc-multilib net-tools vim gdb gdb-multiarch python python3 python3-pip python3-dev libssl-dev libffi-dev wget git make procps libpcre3-dev libdb-dev libxt-dev libxaw7-dev python-pip libc6:i386 libncurses5:i386 libstdc++6:i386 ruby-dev wine64 wine32 wireshark audacity bless zip unzip nasm
 pip install capstone requests pwntools r2pipe ropgadget
-pip3 install pwntools keystone-engine unicorn==1.0.2rc1 capstone ropper ropgadget
-
+pip3 install pwntools keystone-engine unicorn==1.0.2rc1 capstone ropper ropgadget pycrypto
+clear
+echo "Enter Password"
+chsh -s $(which zsh)
 mkdir $HOME/tools 
 cd $HOME/tools
 
@@ -26,6 +28,7 @@ git clone https://github.com/niklasb/libc-database && cd libc-database && ./get
 sudo gem install one_gadget
 # git clone https://github.com/longld/peda.git ~/peda
 # echo "source ~/peda/peda.py" >> ~/.gdbinit
+sudo curl -L http://install.ohmyz.sh | sh
 cd $HOME
 wget https://github.com/yuumi001/sth-weird/blob/main/ubuntu/aliases?raw=true
 mv 'aliases?raw=true' aliases
